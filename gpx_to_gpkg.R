913e8bbc7d94f03e7020156b8597c3ed07394a38
Ward Langeraert <ward.langeraert@inbo.be>
2025-07-06 22:45:50 +0200

    add information manually

--- gpx_to_gpkg.R
+++ gpx_to_gpkg.R
@@ -1,6 +1,7 @@
 library(sf)
 library(tidyverse)
 
+# Not necessary anymore ...
 source("calculate_elevation_gain.R")
 
 # Get gpx files
@@ -23,12 +24,13 @@ tracks_list <- lapply(gpx_files, function(path) {
         date = format(track_date, "%d/%m/%Y"),
         ele_start = elevation_start,
         ele_end = elevation_end,
-        dist = st_length(tracks_df_raw),
-        ascent = calculate_elevation_gain(track_points_df_raw),
-        descent = calculate_elevation_gain(track_points_df_raw, ascent = FALSE),
-        dist_label = paste0(round(as.numeric(dist) / 1000, 2), " km\n",
-                            "(", round(ascent, 1), " m ↑, ",
-                            round(descent, 1), " m ↓", ")"),
+        # Not necessary anymore ...
+        dist2 = st_length(tracks_df_raw),
+        ascent2 = calculate_elevation_gain(track_points_df_raw),
+        descent2 = calculate_elevation_gain(track_points_df_raw, ascent = FALSE),
+        dist_label2 = paste0(round(as.numeric(dist2) / 1000, 2), " km\n",
+                             "(", round(ascent2, 1), " m ↑, ",
+                             round(descent2, 1), " m ↓", ")"),
       ) %>%
       select(everything(), geometry)
     
@@ -36,13 +38,18 @@ tracks_list <- lapply(gpx_files, function(path) {
   }
 )
 
-# Read track names
+# Read track names + correct data
 track_names <- read_csv2("track_names.csv") %>%
-  mutate(date = format(lubridate::dmy(date), "%d/%m/%Y"))
+  mutate(
+    date = format(lubridate::dmy(date), "%d/%m/%Y"),
+    dist_label = paste0(
+      dist, " km (", round(ascent, 1), " m ↑, ",
+      round(descent, 1), " m ↓", ")")
+  )
 
 tracks <- bind_rows(tracks_list) %>%
   left_join(track_names, by = join_by(date)) %>%
-  mutate(label = paste0(nr, ": ", start, " - ", end)) %>%
+  mutate(label = paste0(nr, ": ", start, " - ", end, "\n", dist_label)) %>%
   select(nr, start, end, date, everything(), geometry)
 
 st_write(tracks, "hiking_tracks.gpkg", append = FALSE)
